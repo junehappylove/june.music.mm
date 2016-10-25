7aae8870ea7430fc671e263cce45785367c8fdbf
iscas <iscas@iscas>
2016-10-25 22:33:18 +0800

    donot need to use javafx. if need ,should import the javafx.jar from jdk path

--- src/com/judy/Runner.java
+++ src/com/judy/Runner.java
@@ -7,45 +7,15 @@ package com.judy;
 
 import com.judy.momoplayer.player.ui.Main;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  * 启动音乐播放器 <br>
  * 
  * @author 王俊伟 wjw.happy.love@163.com
  * @date 2016年10月21日 下午9:53:17
  */
-public class Runner extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-
-		Scene scene = new Scene(root, 300, 250);
-
-		primaryStage.setTitle("Hello World!");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-
+public class Runner{
+	
 	public static void main(String[] args) {
-		//launch(args);
 		Main.main(args);
 	}
 }
